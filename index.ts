0a3d58de68beafd1e3a24c746aefd8ab9484e291
Maggy Zhou <maggy.zhou@student-cs.fr>
2025-10-22 14:22:28 +0200

    feat: similarity for maison & appartement

--- index.ts
+++ index.ts
@@ -17,8 +17,8 @@ async function handler(_req: Request): Promise<Response> {
   headers.append("Content-Type", "application/json");
 
   const similarityRequestBody = JSON.stringify({
-    word1: "centrale",
-    word2: "supelec",
+    word1: "maison",
+    word2: "appartement",
   });
 
   const requestOptions = {
